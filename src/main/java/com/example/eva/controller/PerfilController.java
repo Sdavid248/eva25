e6ed3b5372be984b40ebdf2a7e3879cfa36cab4b
Sdavid248 <senatareasetc@gmail.com>
2025-12-03 13:51:26 -0500

    cambios en mapa y en perfilcontroller

---
+++ src/main/java/com/example/eva/controller/PerfilController.java
@@ -0,0 +1,31 @@
+package com.example.eva.controller;
+
+import com.example.eva.model.Usuario;
+import com.example.eva.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PerfilController {
+
+    @GetMapping("/perfil")
+    public String mostrarPerfil(Model model) {
+
+        // Datos simulados (después pueden venir desde BD)
+        model.addAttribute("nombre", "Juan Pérez");
+        model.addAttribute("correo", "juanperez@email.com");
+        model.addAttribute("ubicacion", "Ciudad de México");
+        model.addAttribute("edad", 28);
+        model.addAttribute("ocupacion", "Desarrollador Web");
+        model.addAttribute("intereses", "Programación, videojuegos, música");
+        model.addAttribute("foto", "https://via.placeholder.com/150");
+
+        return "perfil"; // nombre del HTML
+    }
+}
